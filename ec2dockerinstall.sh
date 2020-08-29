412a2cbdd029426360b045f1947f0c96872bc8a5
Vedant Mhatre <52707230+Vedant-Mhatre@users.noreply.github.com>
2020-08-29 13:19:36 +0530

    Update ec2dockerinstall.sh

--- ec2dockerinstall.sh
+++ ec2dockerinstall.sh
@@ -1,9 +1,9 @@
-sudo apt update
+sudo apt -y update
 sudo apt -y install apt-transport-https ca-certificates curl software-properties-common git
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
+sudo apt -y update
 apt-cache policy docker-ce > dockerrepo.txt
-sudo apt install docker-ce
+sudo apt -y install docker-ce
 sudo systemctl status docker > installstatus.txt
 sudo usermod -aG docker ${USER}
